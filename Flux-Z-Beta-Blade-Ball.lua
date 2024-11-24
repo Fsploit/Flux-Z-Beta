c0474192bb4ba6df049dfdec72e6b277f1c1cf56
Fsploit (retarded as fuck) <169143383+Fsploit@users.noreply.github.com>
2024-11-24 17:52:51 +0800

    Update Flux-Z-Beta-Blade-Ball.lua

--- Flux-Z-Beta-Blade-Ball.lua
+++ Flux-Z-Beta-Blade-Ball.lua
@@ -1,8 +1,8 @@
 wait(0.2)
 game.StarterGui:SetCore("SendNotification", {
 Title = "Fluxus Z"; -- the title 
-Text = "Will be back up and running 8pm Philippines time"; -- what the text says 
-Duration = 200; -- how long the notification should in secounds
+Text = "We are back up and running!"; -- what the text says 
+Duration = 5; -- how long the notification should in secounds
 })
 --[[
  .____                  ________ ___.    _____                           __                
